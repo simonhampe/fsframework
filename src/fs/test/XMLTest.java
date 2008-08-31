9cdf0c13c46221b4af9e9376d2abeab6d830c8a6
Simon Hampe <simon.hampe@googlemail.com>
2008-08-31 08:43:31 +0000

    - class LocalizedStringTable added, as well as a xml schema definition and an example file for the internal xml format of this class
    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsframework@8 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/test/XMLTest.java
+++ src/fs/test/XMLTest.java
@@ -16,23 +16,6 @@ import fs.xml.*;
 public class XMLTest {
 
 	public static void main(String[] args) {
-		Config c1 = new Config("test",1);
-		Config c2 = new Config("test",2);
-		try {
-			XMLConfigureFile f = new XMLConfigureFile("xmltest.xml");
-			f.registerConfigurable(c1);
-			f.registerConfigurable(c2);
-			try {
-				f.readConfigurations();
-			}
-			catch(Exception e) {
-				System.out.println(e.getMessage());
-			}
-			f.store();
-		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
-		}	
-		System.out.println(c1 + "\n" + c2);
+		
 	}
 }
---
+++ src/fs/xml/LocalizedStringTable.java
@@ -0,0 +1,59 @@
+package fs.xml;
+
+import org.dom4j.*;
+import org.dom4j.tree.*;
+
+/**
+ * Implements an XML-based String table that can be used to store
+ * e.g. application-related constant strings in different locales.
+ * Strings are accessed via a unique String identifier and a locale 
+ * identifier. The locale identifier can be retrieved from a list
+ * of "supported" locales. Since this is a list maintained solely by
+ * the user, no actual guarantee will be made, that a locale contained in
+ * this list is actually completely supported. Its only purpose is to 
+ * inform other users of potentially available locales.
+ * @author Simon Hampe
+ *
+ */
+public class LocalizedStringTable implements XMLConfigurable {
+
+	/**
+	 * The XML-Document representing the string table
+	 */
+	private Document xmltable;
+	
+	/* *********************************************
+	 * Constructors
+	 */
+	
+	/**
+	 * This constructor will initialize an empty string table
+	 * with no locales
+	 */
+	public LocalizedStringTable() {
+		xmltable = new DefaultDocument(new DefaultElement("locstringtable"));
+	}
+	
+	
+	
+	public void configure(Node n) throws XMLWriteConfigurationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Node getConfiguration() throws XMLReadConfigurationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isConfigured() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
