edbc3c45856732b08ac04859d47464366e6cf963
Simon Hampe <simon.hampe@googlemail.com>
2009-01-31 12:54:08 +0000

    Started to write StringEditor. Noticed that I need to implement Validation mechanism and to work on my reading course...
    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsframework@49 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/polyglot/view/StringEditor.java
+++ src/fs/polyglot/view/StringEditor.java
@@ -1,6 +1,22 @@
 package fs.polyglot.view;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
 import fs.gui.FrameworkDialog;
+import fs.polyglot.model.PolyglotTableModel;
 import fs.xml.PolyglotStringLoader;
 import fs.xml.ResourceReference;
 
@@ -11,11 +27,101 @@ import fs.xml.ResourceReference;
  */
 public class StringEditor extends FrameworkDialog {
 
-	public StringEditor(ResourceReference r, PolyglotStringLoader l, String lid) {
+	//Components
+	private JTextField textID;
+	private JTextField textGroup;
+	private JCheckBox checkGenerateID;
+	private JCheckBox checkQuickNav;
+	private JLabel generatedID;
+	private JTable tableVariants;
+	private JComboBox jumpto;
+	private JButton previous;
+	private JButton next;
+	private JButton config;
+	private JButton ok;
+	private JButton cancel;
+	
+	//Data
+	private PolyglotTableModel table;
+	private ListSelectionModel selection;
+	private StringEditorConfiguration configuration;
+	private static String sgroup = "fs.polyglot.StringEditor";
+	private String currentEdit = null;
+	
+	/**
+	 * This creates a string editor. If singleStringID is not null, this creates an editor for a single string, with navigational controls
+	 * disabled. All configuration parameters are then ignored. If it is null, the editor will create a list of editable strings from the
+	 * configuration data and fill all fields with the data of the first one. Navigational controls are activated, as well as Quick Navigation.
+	 * @param associatedTable The associated Table from which the data should be retrieved. If it is null, a null pointer exception is thrown
+	 * @param selectedStrings A list model from which to retrieve a list of selected strings. If only a single string is to be edited, this parameter is ignored
+	 * @param singleStringID Should be null, if the strings to be edited should be retrieved from the configuration data. Otherwise this stringID is edited and its data
+	 * retrieved from the table model
+	 * @param configuration The Configuration data. This specifies, which strings are to be edited. Usually this data is created by a StringEditorConfigurator.
+	 */
+	public StringEditor(ResourceReference r, PolyglotStringLoader l, String lid, PolyglotTableModel associatedTable, ListSelectionModel selectedStrings, String singleStringID, StringEditorConfiguration configuration) {
 		super(r, l, lid);
-		// TODO Auto-generated constructor stub
+		
+		//Init additional components
+		JLabel labelID = new JLabel(loader.getString(sgroup + ".stringid", languageID));
+		JLabel labelGroup = new JLabel(loader.getString(sgroup + ".groupid", languageID));
+		JLabel labelTable = new JLabel(loader.getString(sgroup + ".variants", languageID));
+		JLabel labelJump = new JLabel(loader.getString(sgroup + ".jumpto", languageID));
+		JScrollPane tablePane = new JScrollPane(tableVariants);
+		
+		//Init all member components
+		textID = new JTextField();
+		textGroup = new JTextField();
+		checkGenerateID = new JCheckBox(loader.getString(sgroup + ".generateid", languageID), true);
+		checkQuickNav = new JCheckBox(loader.getString(sgroup + ".quicknav", languageID),singleStringID == null); checkQuickNav.setEnabled(singleStringID == null);
+		generatedID = new JLabel("");
+			generatedID.setBorder(BorderFactory.createEtchedBorder());
+		tableVariants = new JTable();
+		jumpto = new JComboBox();
+		previous = new JButton("<-");
+		next = new JButton("->");
+		config = new JButton(loader.getString(sgroup + ".config", languageID));
+		ok = new JButton(loader.getString("fs.global.ok", languageID));
+		cancel = new JButton(loader.getString("fs.global.cancel", languageID));
+		
+		//Layout
+		Box lineBox = new Box(BoxLayout.Y_AXIS); //The box for all lines
+		Box line1 = new Box(BoxLayout.X_AXIS);
+		line1.setAlignmentX(LEFT_ALIGNMENT);
+			line1.add(labelID); line1.add(textID); line1.add(labelGroup); line1.add(textGroup);
+		Box line2 = new Box(BoxLayout.X_AXIS);
+		line2.setAlignmentX(LEFT_ALIGNMENT);
+			line2.add(checkGenerateID);line2.add(generatedID);
+		Box line3 = new Box(BoxLayout.X_AXIS);
+		line3.setAlignmentX(LEFT_ALIGNMENT);
+			line3.add(labelTable);
+		Box line4 = new Box(BoxLayout.X_AXIS);
+		line4.setAlignmentX(LEFT_ALIGNMENT);
+			line4.add(tablePane);
+		Box line5 = new Box(BoxLayout.X_AXIS);
+		line5.setAlignmentX(LEFT_ALIGNMENT);
+			line5.add(checkQuickNav);
+		Box line6 = new Box(BoxLayout.X_AXIS);
+		line6.setAlignmentX(LEFT_ALIGNMENT);
+			line6.add(Box.createHorizontalGlue());line6.add(previous); line6.add(config); line6.add(ok); line6.add(cancel); line6.add(next);
+			line6.add(Box.createHorizontalGlue());
+		Box line7 = new Box(BoxLayout.X_AXIS);
+		line7.setAlignmentX(LEFT_ALIGNMENT);
+			line7.add(labelJump); line7.add(jumpto);line7.add(Box.createHorizontalGlue());
+			
+		lineBox.add(line1);lineBox.add(line2);lineBox.add(line3); lineBox.add(line4); lineBox.add(line5); lineBox.add(line6);lineBox.add(line7);
+		add(lineBox);
+		pack();
+		
+		
 	}
 
+	/**
+	 * If only a single string is edited by this instance, this has no effect. Otherwise, the list of edited strings and languages is reloaded. If 
+	 * possible, the current selection is left unchanged and the last edit is performed before the update, if possible.
+	 */
+	public void updateData() {
+		
+	}
 	
 	
 }
--- src/fs/test/StringEditorTest.java
+++ src/fs/test/StringEditorTest.java
@@ -6,7 +6,10 @@ import javax.swing.JDialog;
 
 import org.dom4j.DocumentException;
 
+import fs.event.DataRetrievalListener;
 import fs.polyglot.model.PolyglotTableModel;
+import fs.polyglot.view.StringEditor;
+import fs.polyglot.view.StringEditorConfiguration;
 import fs.polyglot.view.StringEditorConfigurator;
 import fs.xml.XMLToolbox;
 import fs.xml.XMLWriteConfigurationException;
@@ -19,11 +22,21 @@ import fs.xml.XMLWriteConfigurationException;
 public class StringEditorTest {
 
 	public static void main(String[] args) {
-		PolyglotTableModel model;
+		final PolyglotTableModel model;
 		try {
 			model = new PolyglotTableModel(XMLToolbox.loadXMLFile(new File("language/fsfwStringTable.xml")), null);
 			StringEditorConfigurator configurator = new StringEditorConfigurator(null,null,null, model);
-			configurator.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			configurator.addDataRetrievalListener(new DataRetrievalListener() {
+
+				@Override
+				public void dataReady(Object source, Object data) {
+					StringEditorConfiguration config = (StringEditorConfiguration) data;
+					StringEditor editor = new StringEditor(null, null, null, model, null, null,config);
+					editor.setVisible(true);
+					editor.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				}
+				
+			});
 			configurator.setVisible(true);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
