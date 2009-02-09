8a3cdd89b17eac52f725af9a01629b081b9e7204
Simon Hampe <simon.hampe@googlemail.com>
2009-02-09 16:20:03 +0000

    Wrote a table cell editor component. Still unsure about how to implement it (might have to rewrite the constructor)...
    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsframework@59 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/polyglot/view/VariantTableCellEditor.java
+++ src/fs/polyglot/view/VariantTableCellEditor.java
@@ -1,20 +1,25 @@
 package fs.polyglot.view;
 
 import java.awt.Component;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.EventObject;
+import java.util.HashSet;
 
 import javax.swing.AbstractCellEditor;
 import javax.swing.CellEditor;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.event.CellEditorListener;
 import javax.swing.table.TableCellEditor;
 
 import org.dom4j.Document;
 
 import fs.gui.SwitchIconLabel;
+import fs.validate.LabelIndicValidator;
+import fs.validate.ValidationResult.Result;
 import fs.xml.FsfwDefaultReference;
 import fs.xml.PolyglotStringLoader;
 import fs.xml.PolyglotStringTable;
@@ -22,26 +27,61 @@ import fs.xml.ResourceDependent;
 import fs.xml.ResourceReference;
 import fs.xml.XMLDirectoryTree;
 
-public class VariantTableCellEditor extends AbstractCellEditor implements TableCellEditor, ResourceDependent {
+public class VariantTableCellEditor extends JPanel implements TableCellEditor, ResourceDependent {
 
 	//Resources
 	private ResourceReference resource;
 	private PolyglotStringLoader loader;
 	private String languageID;
 	
-	//Icon
-	private ImageIcon warn;
-	
+	//Data
+	private HashSet<String> tabooList = new HashSet<String>(); //List of forbidden language ids
+
 	// CONSTRUCTOR *********************************************
 	// *********************************************************
 	
 	/**
 	 * Constructs an editor, using the specified resource reference and string loader and language ID. (null = default value)
 	 */
-	public VariantTableCellEditor(ResourceReference r, PolyglotStringLoader l, String lang) {
+	public VariantTableCellEditor(ResourceReference r, PolyglotStringLoader l, String lang, HashSet<String> taboos) {
+		super();
 		assignReference(r);
 		loader = l != null ? l : PolyglotStringLoader.getDefaultLoader();
 		languageID = lang != null? lang : PolyglotStringTable.getGlobalLanguageID();
+		if(taboos != null) this.tabooList = new HashSet<String>(taboos);
+		
+		
+		//Init component
+		add(label);
+		add(entryField);
+		
+		//Init listeners
+		nonEmptyValidator = new LabelIndicValidator<JTextField>(null, warn,warn) {
+			@Override
+			protected void registerToComponent(JTextField component) {
+				component.getDocument().addDocumentListener(this);				
+			}
+			@Override
+			protected void unregisterFromComponent(JTextField component) {
+				component.getDocument().addDocumentListener(this);				
+			}
+			@Override
+			public Result validate(JTextField component) {
+				//non empty
+				if(component.getText().trim().equals("")) {
+					setToolTipText(component, loader.getString(sgroup + ".nonempty", languageID));
+					return Result.INCORRECT;
+				}
+				//Not already existing
+				if(tabooList.contains(component.getText()) && !component.getText().equals(oldid)) {
+					setToolTipText(component, loader.getString(sgroup + ".existant", languageID));
+					return Result.INCORRECT;
+				}
+				//Else correct
+				setToolTipText(component, null);
+				return Result.CORRECT;
+			}
+		};
 	}
 	
 	// INTERFACE METHODS ***************************************
@@ -50,39 +90,51 @@ public class VariantTableCellEditor extends AbstractCellEditor implements TableC
 	@Override
 	public Component getTableCellEditorComponent(JTable table, Object value,
 			boolean isSelected, int row, int column) {
-		JPanel p = new JPanel();
+		JPanel editorPanel = new JPanel();
 		SwitchIconLabel label = new SwitchIconLabel(warn);
-		
-		return p;
+		JTextField entryField = new JTextField();
+		//If it is a language id, we have to validate the entry
+		if(row == 0) {
+			//Extract the current value, which will be of course be allowed as value
+			final String oldid = table.getValueAt(row, column).toString();
+			LabelIndicValidator<JTextField> langValidator = new LabelIndicValidator<JTextField>(null, warn,warn) {
+				@Override
+				protected void registerToComponent(JTextField component) {
+					component.getDocument().addDocumentListener(this);
+				}
+				@Override
+				protected void unregisterFromComponent(JTextField component) {
+					component.getDocument().removeDocumentListener(this);
+				}
+				@Override
+				public Result validate(JTextField component) {
+					//non empty
+					if(component.getText().trim().equals("")) {
+						setToolTipText(component, loader.getString(sgroup + ".nonempty", languageID));
+						return Result.INCORRECT;
+					}
+					//Not already existing
+					if(tabooList.contains(component.getText()) && !component.getText().equals(oldid)) {
+						setToolTipText(component, loader.getString(sgroup + ".existant", languageID));
+						return Result.INCORRECT;
+					}
+					//Else correct
+					setToolTipText(component, null);
+					return Result.CORRECT;
+				}
+			};
+		}
+		editorPanel.add(label);
+		editorPanel.add(entryField);
+		return editorPanel;
 	}
 	
-	@Override
-	public void cancelCellEditing() {
-	}
-
 	@Override
 	public Object getCellEditorValue() {
 		return null;
 	}
+	
 
-	@Override
-	public boolean isCellEditable(EventObject anEvent) {
-		return false;
-	}
-
-	@Override
-	public void removeCellEditorListener(CellEditorListener l) {
-	}
-
-	@Override
-	public boolean shouldSelectCell(EventObject anEvent) {
-		return false;
-	}
-
-	@Override
-	public boolean stopCellEditing() {
-		return false;
-	}
 
 	// RESOURCEDEPENDENT METHODS ****************************************
 	// ******************************************************************
@@ -106,6 +158,6 @@ public class VariantTableCellEditor extends AbstractCellEditor implements TableC
 		return tree;
 	}
 
-	
+		
 
 }
---
+++ src/fs/polyglot/view/VariantTableEditor.java
@@ -0,0 +1,150 @@
+package fs.polyglot.view;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashSet;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
+
+import org.dom4j.Document;
+
+import fs.event.DataRetrievalListener;
+import fs.gui.SwitchIconLabel;
+import fs.polyglot.validate.TabooValidator;
+import fs.validate.ValidationResult.Result;
+import fs.xml.FsfwDefaultReference;
+import fs.xml.PolyglotStringLoader;
+import fs.xml.PolyglotStringTable;
+import fs.xml.ResourceDependent;
+import fs.xml.ResourceReference;
+import fs.xml.XMLDirectoryTree;
+
+/**
+ * Implements the component which is used for editing cells in a variant table. It is created configured via the corresponding 
+ * VariantTableCellEditor
+ * @author hampe
+ *
+ */
+public class VariantTableEditor extends JPanel implements ResourceDependent {
+
+	/**
+	 * compiler-generated version id 
+	 */
+	private static final long serialVersionUID = 4549677866383774748L;
+	
+	//Resources
+	private ResourceReference resource;
+	private PolyglotStringLoader loader;
+	private String languageID;
+	private static String sgroup = "fs.polyglot.VariantTableEditor";
+	
+	//Components
+	private SwitchIconLabel label;
+	private JTextField entryField;
+	
+	//Icon
+	private ImageIcon warn;
+	
+	//Data
+	private HashSet<String> tabooList;
+	
+	//Listeners
+	private HashSet<DataRetrievalListener> listeners = new HashSet<DataRetrievalListener>();
+	
+	private TabooValidator languageValidator;
+	
+	private KeyListener entryListener = new KeyAdapter() {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			super.keyPressed(e);
+			//If ENTER is pressed and the content is valid, notify of an edit stop
+			if(e.getKeyCode() == KeyEvent.VK_ENTER && languageValidator.validate().getOverallResult() == Result.CORRECT) {
+				fireDataReady(entryField, entryField.getText());
+			}
+			//Id ESCAPE is pressed, 
+			if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+				fireDataReady(entryField, null);
+			}
+		}	
+	};
+	
+	// CONSTRUCTOR ******************************************************
+	// ******************************************************************
+	
+	/**
+	 * Constructs an editor, that is a panel containing a SwitchIconLabel and a text field. The cell editor is notified of edit stop / cancel
+	 * via a dataReady call. Edit calls are allowed /disallowed according to the validation status. An edit cancel is represented by a null data object,
+	 * an edit stop by the string which is the content.
+	 * @param taboos If this is null, every possible content is valid content. Otherwise the content may not be empty and not be contained in this list
+	 */
+	public VariantTableEditor(HashSet<String> taboos, ResourceReference r, PolyglotStringLoader l, String lang) {
+		super();
+		assignReference(r);
+		loader = l != null ? l : PolyglotStringLoader.getDefaultLoader();
+		languageID = lang != null? lang : PolyglotStringTable.getGlobalLanguageID();
+		tabooList = taboos == null? taboos : new HashSet<String>(taboos);
+		
+		//Init components
+		add(label); add(entryField);
+		languageValidator = (taboos == null)? new TabooValidator(null,null,null,new HashSet<String>(),true) : 
+							new TabooValidator(null,warn,warn,tabooList,false) {
+			@Override
+			public Result validate(JTextComponent component) {
+				Result r = super.validate(component);
+				//Set Tooltips
+				switch(r) {
+				case INCORRECT: if(entryField.getText().trim().equals("")) setToolTipText(component,loader.getString(sgroup + ".nonempty", languageID));
+								else setToolTipText(component, loader.getString(loader + ".existant", languageID)); break;
+				default: setToolTipText(component, null);
+				}
+				return r;
+			}			
+		};
+		languageValidator.addComponent(entryField, label);
+		entryField.addKeyListener(entryListener);
+		
+	}
+	
+	// RESOURCEDEPENDENT METHODS ****************************************
+	// ******************************************************************
+	
+	/**
+	 * Assigns a reference (if null, uses the default reference) and reloads the icon
+	 */
+	@Override
+	public void assignReference(ResourceReference r) {
+		resource = r != null? r : FsfwDefaultReference.getDefaultReference();
+		warn = new ImageIcon(resource.getFullResourcePath(this, "graphics/VariantTableEditor/warn.png"));
+	}
+
+	/**
+	 * Expects the icon warn.png in its graphics directory
+	 */
+	@Override
+	public Document getExpectedResourceStructure() {
+		XMLDirectoryTree tree = new XMLDirectoryTree();
+		tree.addPath("graphics/VariantTableEditor/warn.png");
+		return tree;
+	}
+
+	// LISTENER METHODS ********************************************
+	// *************************************************************
+	
+	public void addDataRetrievalListener(DataRetrievalListener l) {
+		if(l != null) listeners.add(l);
+	}
+	
+	public void removeDataRetrievalListener(DataRetrievalListener l) {
+		listeners.remove(l);
+	}
+	
+	protected void fireDataReady(Object source, Object data) {
+		for(DataRetrievalListener l : listeners) l.dataReady(source, data);
+	}
+	
+	
+}
