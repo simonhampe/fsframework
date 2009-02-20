a8dec37bd5633c36f1a6b300e2821e58d6ceff58
Simon Hampe <simon.hampe@googlemail.com>
2009-02-20 10:28:50 +0000

    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsframework@72 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/polyglot/view/TableEditPane.java
+++ src/fs/polyglot/view/TableEditPane.java
@@ -1,11 +1,19 @@
 package fs.polyglot.view;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import org.dom4j.Document;
 
+import fs.event.DocumentChangeFlag;
+import fs.polyglot.model.PolyglotTableModel;
+import fs.test.XMLDirectoryTest;
+import fs.xml.FsfwDefaultReference;
+import fs.xml.PolyglotStringLoader;
+import fs.xml.PolyglotStringTable;
 import fs.xml.ResourceDependent;
 import fs.xml.ResourceReference;
+import fs.xml.XMLDirectoryTree;
 
 /**
  * Implements a three-view panel for editing polyglot string tables. The three views
@@ -19,16 +27,57 @@ public class TableEditPane extends JPanel implements ResourceDependent {
 	// VIEW ***************************************
 	// ********************************************
 	
+	private StringTreeView stringtree;
+	private GroupTreeView  grouptree;
+	private LanguageListView languagelist;
+	private JTextField tableid;
+	private JTextField tabledesc;
+	
+	// DATA ***************************************
+	// ********************************************
+	
+	//Table data
+	private PolyglotTableModel table;
+	private DocumentChangeFlag flag;
+	
+	//Resource
+	private ResourceReference resource;
+	private PolyglotStringLoader loader;
+	private String languageID;
+	
 	// CONSTRUCTOR ********************************
 	// ********************************************
 	
+	public TableEditPane(PolyglotTableModel model, ResourceReference r, PolyglotStringLoader l, String langID) {
+		//Copy data
+		assignReference(r);
+		this.languageID = langID != null? langID : PolyglotStringTable.getGlobalLanguageID();
+		loader = l!= null? l : PolyglotStringLoader.getDefaultLoader();
+		
+		
+	}
+	
+	// RESOURCEDEPENDENT **************************
+	// ********************************************
+	
+	/**
+	 * Assigns a new resource reference. If r == null, the default reference is used.
+	 * Since the resource references for contained components are not updated, it doesn't
+	 * make much sense to call this method after initialization.
+	 */
 	@Override
 	public void assignReference(ResourceReference r) {
+		resource = r!= null? r : FsfwDefaultReference.getDefaultReference();
 	}
 
+	/**
+	 * Returns a document containing the resource needs of all contained components.
+	 */
 	@Override
 	public Document getExpectedResourceStructure() {
-		return null;
+		XMLDirectoryTree tree = new XMLDirectoryTree();
+		
+		return tree;
 	}
 
 }
