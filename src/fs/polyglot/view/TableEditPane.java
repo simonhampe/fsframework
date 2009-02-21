a5e96f780dd2747cc717130f0c6ad4717a73594a
Simon Hampe <simon.hampe@googlemail.com>
2009-02-21 11:32:06 +0000

    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsframework@74 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/polyglot/view/TableEditPane.java
+++ src/fs/polyglot/view/TableEditPane.java
@@ -1,13 +1,23 @@
 package fs.polyglot.view;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.HashSet;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import org.dom4j.Document;
 
 import fs.event.DocumentChangeFlag;
+import fs.gui.GUIToolbox;
+import fs.gui.SwingAppender;
 import fs.polyglot.model.PolyglotTableModel;
 import fs.test.XMLDirectoryTest;
 import fs.xml.FsfwDefaultReference;
@@ -33,7 +43,9 @@ public class TableEditPane extends JPanel implements ResourceDependent {
 	private GroupTreeView  grouptree;
 	private LanguageListView languagelist;
 	private JTextField tableid;
-	private JTextField tabledesc;
+	private JTextArea tabledesc;
+	private SwingAppender logAppender;
+	private JProgressBar progressBar;
 	
 	// DATA ***************************************
 	// ********************************************
@@ -46,6 +58,7 @@ public class TableEditPane extends JPanel implements ResourceDependent {
 	private ResourceReference resource;
 	private PolyglotStringLoader loader;
 	private String languageID;
+	private final static String sgroup = "fs.polyglot.TableEditPane";
 	
 	// CONSTRUCTOR ********************************
 	// ********************************************
@@ -62,7 +75,44 @@ public class TableEditPane extends JPanel implements ResourceDependent {
 		stringtree = new StringTreeView(resource, loader, languageID,table);
 		grouptree = new GroupTreeView(resource, loader, languageID, table);
 		languagelist = new LanguageListView(resource,loader,languageID, table);
+		tableid	= new JTextField(table.getTableID());
+		tabledesc = new JTextArea(table.getTableDescription());
+		logAppender = new SwingAppender(null,resource,loader, languageID);
+		progressBar = new JProgressBar();
 		
+		JPanel headerPanel = new JPanel();
+		JPanel editPanel = new JPanel();
+		JPanel statusBar = new JPanel();
+		JSplitPane horizontalPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,headerPanel,editPanel);
+		
+		JLabel tableidlabel = new JLabel(loader.getString(sgroup + ".idlabel", languageID));
+		JLabel tabledesclabel = new JLabel(loader.getString(sgroup + ".desclabel", languageID));
+				
+		//Layout ------------------------------------------
+		
+		//Header Panel
+		Box headerBox = new Box(BoxLayout.Y_AXIS);
+		Box idbox = new Box(BoxLayout.X_AXIS);
+			idbox.setAlignmentX(Box.LEFT_ALIGNMENT);
+			idbox.add(tableidlabel);idbox.add(tableid);
+		Box descbox = new Box(BoxLayout.X_AXIS);
+			descbox.setAlignmentX(Box.LEFT_ALIGNMENT);
+			descbox.add(tabledesclabel);descbox.add(tabledesc);
+		headerPanel.add(headerBox);
+		
+		//Edit panel
+		GridBagLayout gbl = new GridBagLayout();
+		editPanel.setLayout(gbl);
+		GridBagConstraints gcString = GUIToolbox.buildConstraints(0, 0, 1, 2);
+			gcString.weightx = 100;
+		GridBagConstraints gcLang = GUIToolbox.buildConstraints(1, 0, 1, 1);
+		GridBagConstraints gcGroup = GUIToolbox.buildConstraints(1, 1, 1, 1);
+		gbl.setConstraints(stringtree, gcString);
+		gbl.setConstraints(languagelist, gcLang);
+		gbl.setConstraints(grouptree, gcGroup);
+		editPanel.add(stringtree);
+		editPanel.add(languagelist);
+		editPanel.add(grouptree);
 		
 		//Init Eventhandling ------------------------------
 		
